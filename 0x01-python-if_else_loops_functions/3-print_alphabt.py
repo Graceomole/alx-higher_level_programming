9c344937380807ef40e28469d2a12a8492800a74
Graceomole <graceomoleye123@gmail.com>
2022-12-05 21:47:59 +0100

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(97,123):
-    if chr(letter) != '101' and chr(letter) != '113':
-        print("{}".format(chr(letter)), end="")
-     
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
+             
