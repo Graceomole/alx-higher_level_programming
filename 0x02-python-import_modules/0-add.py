d0e18e323ddb8dd75aeb1bbd935d7e382b0aff12
Graceomole <graceomoleye123@gmail.com>
2022-12-07 22:11:52 +0100

    new file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-print("{} + {} = {}".format(1, 2, add(1,2)))
+def main():
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
